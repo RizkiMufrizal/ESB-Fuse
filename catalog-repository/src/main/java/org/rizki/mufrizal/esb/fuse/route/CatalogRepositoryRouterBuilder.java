73e01562109b985ca7708411e7fe6d716640bbe3
rizki mufrizal <mufrizalrizki@gmail.com>
2019-02-24 00:34:11 +0700

    Update generate response

--- catalog-repository/src/main/java/org/rizki/mufrizal/esb/fuse/route/CatalogRepositoryRouterBuilder.java
+++ catalog-repository/src/main/java/org/rizki/mufrizal/esb/fuse/route/CatalogRepositoryRouterBuilder.java
@@ -2,6 +2,11 @@ package org.rizki.mufrizal.esb.fuse.route;
 
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
+import org.rizki.mufrizal.esb.fuse.helper.JsonObject;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
@@ -17,7 +22,26 @@ public class CatalogRepositoryRouterBuilder extends RouteBuilder {
     public void configure() throws Exception {
         from("direct-vm:catalog-repository-save-catalog")
                 .log(LoggingLevel.INFO, "logging ${body}")
-                .to("sql:INSERT INTO tb_catalog(catalog_id, catalog_name, catalog_price) VALUES (:#catalogId, :#catalogName, :#catalogPrice)")
+                .to("sql:INSERT INTO tb_catalog(catalog_id, catalog_name, catalog_price) VALUES (:#CatalogId, :#CatalogName, :#CatalogPrice)")
+                .process(exchange -> {
+                    JsonObject jsonObject = exchange.getIn().getBody(JsonObject.class);
+
+                    Map<String, Object> objectMap = new HashMap<>();
+                    objectMap.put("ResponseTime", new Date());
+                    objectMap.put("ResponseMessage", "Success");
+                    objectMap.put("ResponseCode", "MW-000");
+
+                    Map<String, Object> objectMapData = new HashMap<>();
+                    objectMapData.put("CatalogId", jsonObject.getString("CatalogId"));
+                    objectMapData.put("CatalogName", jsonObject.getString("CatalogName"));
+                    objectMapData.put("CatalogPrice", jsonObject.getString("CatalogPrice"));
+
+                    Map<String, Object> objectMapResponse = new HashMap<>();
+                    objectMapResponse.put("Data", objectMapData);
+                    objectMapResponse.put("Response", objectMap);
+
+                    exchange.getIn().setBody(objectMapResponse);
+                })
                 .end();
     }
 }
